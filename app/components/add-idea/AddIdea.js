4804d3bd58ddce714132527a41f69fc8595b0f7f
Jaroslaw Kisiolek <kisiolek.jaroslaw@gmail.com>
2018-05-15 14:36:09 +0200

    Styles for form

--- app/components/add-idea/AddIdea.js
+++ app/components/add-idea/AddIdea.js
@@ -12,19 +12,37 @@ export default class AddIdea extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.addIdea}>
-        <input name="title" placeholder="Title" type="text" onChange={this.handleChange} />
-        <input
-          name="shortDescription"
-          placeholder="Short description"
-          type="text"
-          onChange={this.handleChange}
-        />
-        <div contentEditable={true} placeholder="Description" onInput={this.handleAreaChange} />
-        <input name="image" placeholder="Image url" type="text" onChange={this.handleChange} />
-        <input name="target" placeholder="Kudo cap " type="number" onChange={this.handleChange} />
-        <button type="submit">Add</button>
-      </form>
+      <div className="center-block" style={{width: 500}}>
+        <form onSubmit={this.addIdea}>
+          <div className="form-group">
+            <label for="">Title</label>
+            <input name="title" className="form-control" type="text" onChange={this.handleChange} />
+          </div>
+          <div className="form-group">
+            <label for="" className="control-label">Short description</label>
+            <input
+              name="shortDescription"
+              className="form-control"
+              type="text"
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <label for="" className="control-label">Description</label>
+            <div contentEditable={true} className="form-control" style={{height: 100}}onInput={this.handleAreaChange} />
+          </div>
+          
+          <div className="form-group">
+            <label for="" className="control-label">Image url</label>
+            <input name="image" className="form-control" type="text" onChange={this.handleChange} />
+          </div>
+          <div className="form-group">
+            <label for="" className="control-label">Kudo cap</label>
+            <input name="target" className="form-control"  type="number" onChange={this.handleChange} />
+          </div>
+          <button type="submit" className="btn button btn-block btn-lg">Add</button>
+        </form>
+      </div>
     )
   }
 }
