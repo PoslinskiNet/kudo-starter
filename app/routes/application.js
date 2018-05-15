07a65a450f00764fff4ec4096f2561c53f7e6b08
Dawid Pośliński <dposlinski@gmail.com>
2018-05-15 12:44:41 +0200

    Current user

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,12 +5,6 @@ export default Route.extend({
   session: service(),
 
   beforeModel() {
-    this.get('session').set('user', {
-      avatarUrl: "https://lh5.googleusercontent.com/-lWm6H2F14S4/AAAAAAAAAAI/AAAAAAAAAGw/I7wWZ51C0CA/photo.jpg",
-      id : "L4rZFdUfAnO9y96feaJ5aOJg98L2",
-      name: "Dawid Pośliński"
-    });
-
     // TODO: uncomment when ready
     // this.transitionTo(this.get('session.user') ? 'ideas' : 'login');
   }
