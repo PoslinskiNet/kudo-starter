e286a0f54fcbdeb28db6b3a86aa33753d5966a32
Damian Sznajder <damsznajder@gmail.com>
2018-05-15 12:53:21 +0200

    Fetch Profile info from auth

--- app/components/profile-top/ProfileTop.js
+++ app/components/profile-top/ProfileTop.js
@@ -2,24 +2,19 @@ import React from 'react'
 
 export default class ProfileTop extends React.Component {
   state = {
-    user: {
-      name: 'Chuje wypusccie mnie',
-      avatar:
-        'https://lh3.googleusercontent.com/-G2hqd2WqBEY/WqE4nyUG3JI/AAAAAAAAAHE/9_Pvr67dMREkMhuukuZt7gsIZWWG6hHRQCEwYBhgL/w280-h280-p/slackimg.png',
-    },
     kudosGiven: 12,
   }
 
   render() {
+    const { kudosGiven } = this.state
     const {
-      user: { avatar, name },
-      kudosGiven,
-    } = this.state
+      user: { avatarUrl, name },
+    } = this.props
 
     return (
       <div className="profile flex-column flex-align-center">
         <div className="avatar">
-          <img className="avatar__image" src={avatar} />
+          <img className="avatar__image" alt="avatar" src={avatarUrl} />
         </div>
         <div className="profile__name">{name}</div>
         <div>{kudosGiven}/20</div>
--- app/components/profile-top/component.js
+++ app/components/profile-top/component.js
@@ -1,9 +1,18 @@
 import ReactComponent from 'kudo-starter/components/base/react-component'
-import ProfileTop from './ProfileTop';
+import ProfileTop from './ProfileTop'
+import { inject as service } from '@ember/service'
 
 export default ReactComponent.extend({
+  session: service(),
+
   didInsertElement() {
     this._super(...arguments)
-    this.reactRender(<ProfileTop />)
+    this.renderProfile()
+  },
+
+  renderProfile() {
+    const user = this.get('session.currentUser')
+
+    this.reactRender(<ProfileTop user={user} />, this.element)
   },
 })
