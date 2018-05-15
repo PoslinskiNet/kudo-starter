18fc9966ee442e2c8665fc0f2029703feaa87efd
Dawid Pośliński <dposlinski@gmail.com>
2018-05-15 12:46:39 +0200

    Current user

---
+++ app/services/session.js
@@ -0,0 +1,9 @@
+import Service from '@ember/service';
+
+export default Service.extend({
+  currentUser: {
+    avatarUrl: "https://lh5.googleusercontent.com/-lWm6H2F14S4/AAAAAAAAAAI/AAAAAAAAAGw/I7wWZ51C0CA/photo.jpg",
+    id : "L4rZFdUfAnO9y96feaJ5aOJg98L2",
+    name: "Dawid Pośliński"
+  }
+});
