d10c3d3217660c345ed096c3295b750650fa0845
Damian Sznajder <damsznajder@gmail.com>
2018-05-15 13:29:56 +0200

    Add routing for add idea

--- app/components/add-idea/AddIdea.js
+++ app/components/add-idea/AddIdea.js
@@ -1,21 +1,31 @@
 import React from 'react'
 
 export default class AddIdea extends React.Component {
-  state = {};
-  handleChange = ({ target: { name, value }}) => this.setState({[name]: value});
-  addIdea = (event) => {
-    event.preventDefault();
-    this.props.addIdea(this.state);
+  state = {}
+  handleChange = ({ target: { name, value } }) => this.setState({ [name]: value })
+  addIdea = event => {
+    event.preventDefault()
+    this.props.addIdea(this.state)
   }
 
   render() {
     return (
       <form onSubmit={this.addIdea}>
-        <input name="title" placeholder="title" type="text" onChange={this.handleChange} />
-        <input name="shortDescription" placeholder="Short description" type="text" onChange={this.handleChange} />
-        <input name="description" placeholder="Description" type="textarea" onChange={this.handleChange} />
-        <input name="image" type="text" onChange={this.handleChange} />
-        <input name="target" type="number" onChange={this.handleChange} />
+        <input name="title" placeholder="Title" type="text" onChange={this.handleChange} />
+        <input
+          name="shortDescription"
+          placeholder="Short description"
+          type="text"
+          onChange={this.handleChange}
+        />
+        <input
+          name="description"
+          placeholder="Description"
+          type="textarea"
+          onChange={this.handleChange}
+        />
+        <input name="image" placeholder="Image url" type="text" onChange={this.handleChange} />
+        <input name="target" placeholder="Kudo cap " type="number" onChange={this.handleChange} />
         <button type="submit">Add</button>
       </form>
     )
