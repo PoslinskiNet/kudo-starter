26c9389356a64e2b24f91298407801de6a6912bc
Damian Sznajder <damsznajder@gmail.com>
2018-05-15 13:02:48 +0200

    Add support buttons

--- app/components/list-component/Support.js
+++ app/components/list-component/Support.js
@@ -2,26 +2,53 @@ import React from 'react'
 
 export default class Support extends React.PureComponent {
   state = {
+    value: 1,
     showButton: true,
   }
 
-  changeState = () => this.setState({ showButton: !this.state.showButton })
+  toggleSupport = () => this.setState({ showButton: !this.state.showButton })
+
+  sendKudos = () => {
+    const { showButton, value } = this.state
+
+    this.props.giveKudos(value)
+    this.setState({ showButton: !showButton })
+  }
+
+  selectAmount = ({ target: { value } }) => this.setState({ value })
 
   render() {
     const { showButton } = this.state
+
     return (
       <div>
         {showButton ? (
-          <button onClick={this.changeState} className="btn btn-success">
+          <button onClick={this.toggleSupport} className="btn btn-success">
             $upport
           </button>
         ) : (
           <div>
-            <span>$ $ $ $ $</span>
-            <button onClick={this.props.giveKudos} className="btn btn-success">
+            <span>
+              <button value="1" onClick={this.selectAmount}>
+                $
+              </button>
+              <button value="2" onClick={this.selectAmount}>
+                $
+              </button>
+              <button value="3" onClick={this.selectAmount}>
+                $
+              </button>
+              <button value="4" onClick={this.selectAmount}>
+                $
+              </button>
+              <button value="5" onClick={this.selectAmount}>
+                $
+              </button>
+            </span>
+            <button onClick={this.sendKudos} className="btn btn-success">
               Accept
             </button>
-            <button onClick={this.changeState} className="btn btn-success">
+            <button onClick={this.toggleSupport} className="btn btn-success">
               Decline
             </button>
           </div>
