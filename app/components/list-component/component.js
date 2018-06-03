b3be8338ffb55d0bb3ea4ecde1641c58a6585741
Dawid Pośliński <dposlinski@gmail.com>
2018-06-03 17:02:28 +0200

    Convert list & idea component

--- app/components/list-component/component.js
+++ app/components/list-component/component.js
@@ -1,33 +1,21 @@
-import ReactComponent from 'kudo-starter/components/base/react-component';
-import List from './List';
+import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 
-export default ReactComponent.extend({
+export default Component.extend({
   store: service(),
-  session: service(),
 
-  didInsertElement() {
-    this.renderList();
-  },
-
-  didReceiveAttrs() {
+  init() {
     this._super(...arguments);
 
-    if (this.get('component')) {
-      this.get('component').forceUpdate();
-    }
-  },
-
-  renderList() {
-    const ideas = this.get('ideas');
-    const data = ideas.map(idea => {
-      console.log(
-        idea.get('inventorId')
-          ? this.get('store')
-              .findRecord('user', idea.get('inventorId'))
-              .get('avatarUrl')
-          : undefined,
-      );
+    const ideasData = this.get('store').findAll('idea');
+    const ideas = ideasData.map(idea => {
+      // console.log(
+      //   idea.get('inventorId')
+      //     ? this.get('store')
+      //         .findRecord('user', idea.get('inventorId'))
+      //         .get('avatarUrl')
+      //     : undefined,
+      // );
 
       return {
         id: idea.get('id'),
@@ -44,27 +32,6 @@ export default ReactComponent.extend({
       };
     });
 
-    const component = this.reactRender(
-      <List ideas={data} giveKudos={this.actions.giveKudos.bind(this)} />,
-      this.element,
-    );
-
-    this.set('component', component);
-
-    return component;
-  },
-
-  actions: {
-    giveKudos(id, amount) {
-      const store = this.get('store');
-      const user = this.get('session.currentUser');
-      const idea = store.peekRecord('idea', id);
-
-      return this.get('store').createRecord('donation', {
-        user,
-        idea,
-        amount,
-      });
-    },
-  },
+    this.set('ideas', ideas);
+  }
 });
---
+++ app/components/list-component/idea/component.js
@@ -0,0 +1,23 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+const defaultIdeaImage = 'http://www.garstangicecreamfestival.co.uk/wp-content/uploads/2017/11/pexels-photo-461430-360x240.jpeg'
+const defaultInventorImage = 'https://www.fancyhands.com/images/default-avatar-250x250.png';
+
+export default Component.extend({
+  classNames: ['col-md-4'],
+
+  inventorImage: computed('idea.avatarUrl', function() {
+    return this.get('idea.avatarUrl') || defaultInventorImage;
+  }),
+
+  ideaImage: computed('idea.image', function() {
+    return this._isValidURL(this.get('idea.image')) ? this.get('idea.image') : defaultIdeaImage;
+  }),
+
+  _isValidURL(str) {
+    const a = document.createElement('a');
+    a.href = str;
+    return a.host && a.host != window.location.host;
+  }
+});
